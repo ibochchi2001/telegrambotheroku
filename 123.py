6ea7c75901b7ad4c5a3ab7dfbaf008bcdefb1771
ibochchi2001 <46477510+ibochchi2001@users.noreply.github.com>
2019-01-14 18:32:47 +0500

    Update 123.py

--- 123.py
+++ 123.py
@@ -15,6 +15,20 @@ def vaqt(message):
     b=bs4.BeautifulSoup(s.text, "html.parser")
     p3=b.select('#tc1')
     Tong=p3[0].getText()
-    bot.send_message(message.from_user.id, "Vaqt {}".format(Tong))
+    
+    p4=b.select('#tc2')
+    Quyosh=p4[0].getText()
+    
+    p5=b.select('#tc3')
+    Peshin=p5[0].getText()
+    
+    p6=b.select('#tc4')
+    Asr=p6[0].getText()
+    
+    p7=b.select('#tc5')
+    Shom=p7[0].getText()
+    p8=b.select('#tc6')
+    Xufton=p8[0].getText()
+    bot.send_message(message.from_user.id, "**Vaqt**\nTong {}\nQuyosh: {}\nPeshin: {}\nAsr: {}\nShom: {}\nXufton: {}".format(Tong,Quyosh,Peshin,Asr,Shom,Xufton))
 
 bot.polling(none_stop="True", interval=0)
