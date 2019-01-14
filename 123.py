18ea68ec449811e4ca70e42e07b4c2693939ed5f
ibochchi2001 <46477510+ibochchi2001@users.noreply.github.com>
2019-01-14 17:55:50 +0500

    Update 123.py

--- 123.py
+++ 123.py
@@ -9,7 +9,7 @@ def hendle_start(message):
     some_text = "Hello"
     bot.send_message(message.from_user.id, some_text)
 @bot.message_handler(commands=['vaqt'])
-def vaqt(message)L
+def vaqt(message):
     bot.send_message(message.chat.id, "Vaqt")
 
 bot.polling(none_stop="True", interval=0)
