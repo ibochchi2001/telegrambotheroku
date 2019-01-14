3584a55d09e311204bf72696c5132df379940eb6
ibochchi2001 <46477510+ibochchi2001@users.noreply.github.com>
2019-01-14 17:55:09 +0500

    Update 123.py

--- 123.py
+++ 123.py
@@ -8,5 +8,8 @@ bot = telebot.TeleBot("718228299:AAF8CMQLYXHzIEqtyLjIflkYdnJYfzhsxF4")
 def hendle_start(message):
     some_text = "Hello"
     bot.send_message(message.from_user.id, some_text)
+@bot.message_handler(commands=['vaqt'])
+def vaqt(message)L
+    bot.send_message(message.chat.id, "Vaqt")
 
 bot.polling(none_stop="True", interval=0)
