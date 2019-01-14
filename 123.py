071c56b2e8f0c439f84d575588890117cde8c85c
ibochchi2001 <46477510+ibochchi2001@users.noreply.github.com>
2019-01-14 16:05:38 +0500

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,36 +1,25 @@
+import flask
+from telebot import types
+from config import *
+from bot_handlers import bot
 import os
-
-from flask import Flask, request
-
-import telebot
-
-TOKEN = '718228299:AAF8CMQLYXHzIEqtyLjIflkYdnJYfzhsxF4'
-bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
-
+ 
+server = flask.Flask(__name__)
+ 
+ 
 @server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+def get_message():
+    bot.process_new_updates([types.Update.de_json(
+         flask.request.stream.read().decode("utf-8"))])
     return "!", 200
-
-
-@server.route("/")
-def webhook():
+ 
+ 
+@server.route('/', methods=["GET"])
+def index():
     bot.remove_webhook()
-    bot.set_webhook(url='https://salombot.herokuapp.com/' + TOKEN)
-    return "!", 200
-
-
+    bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
+    return "Hello from Heroku!", 200
+ 
+ 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
