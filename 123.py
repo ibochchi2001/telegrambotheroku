b230c21f81d30bac22f4704316bf90d57dfdbb8b
ibochchi2001 <46477510+ibochchi2001@users.noreply.github.com>
2019-01-14 17:50:59 +0500

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,31 +1,12 @@
-import time
-import random
-import datetime
-import telepot
+import telebot
+from telebot import types
 
-"""
-A simple bot that accepts two commands:
-- /roll : reply with a random integer between 1 and 6, like rolling a dice.
-- /time : reply with the current time, like a clock.
-INSERT TOKEN below in the code, and run:
-$ python diceyclock.py
-Ctrl-C to kill.
-"""
+bot = telebot.TeleBot("718228299:AAF8CMQLYXHzIEqtyLjIflkYdnJYfzhsxF4")
 
-def handle(msg):
-    chat_id = msg['chat']['id']
-    command = msg['text']
 
-    print 'Got command: %s' % command
+@bot.message_handler(commands=['start'])
+def hendle_start(message):
+    some_text = "Hello"
+    bot.send_message(message.from_user.id, some_text)
 
-    if command == '/roll':
-        bot.sendMessage(chat_id, random.randint(1,6))
-    elif command == '/time':
-        bot.sendMessage(chat_id, str(datetime.datetime.now()))
-
-bot = telepot.Bot('718228299:AAF8CMQLYXHzIEqtyLjIflkYdnJYfzhsxF4')
-bot.message_loop(handle)
-print 'I am listening ...'
-
-while 1:
-    time.sleep(10)
+bot.polling(none_stop="True", interval=0)
