af2a1afa634178573097eaba84aec20a586e563f
ibochchi2001 <46477510+ibochchi2001@users.noreply.github.com>
2019-01-14 15:53:32 +0500

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,79 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This is a simple echo bot using decorators and webhook with flask
+# It echoes any incoming text messages and does not use the polling method.
+
+import logging
+import time
+
+import flask
+
 import telebot
-import os
-import constants
 
-bot = telebot.TeleBot(constants.token)
+API_TOKEN = '718228299:AAF8CMQLYXHzIEqtyLjIflkYdnJYfzhsxF4'
 
-print(bot.get_me())
+WEBHOOK_HOST = 'https://salombot.herokuapp.com/'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-def log (message , answer):
-    print("\n ----------")
-    from datetime import  datetime
-    print(datetime.now())
-    print("Сообщение от {0} {1}. (id = {2}) \n Текст - {3}".format(message.from_user.first_name,
-                                                                   message.from_user.last_name,
-                                                                   str(message.from_user.id),
-                                                                       message.text))
-    print(answer)
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
-@bot.message_handler(commands=['help'])
-def handle_text(message):
-    answer = "Я ничего не умею :C"
-    log(message,answer)
-    bot.send_message(message.chat.id, """Я ничего не умею :C""")
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
 
-@bot.message_handler(commands=['settings'])
-def handle_text(message):
-    answer = "Я и так идиален ! "
-    log(message, answer)
-    bot.send_message(message.chat.id, "Я и так идиален ! ")
-@bot.message_handler(commands=['start'])
-def handle_text(message):
-    answer = "Я и так был уже запужен"
-    log(message,answer)
-    bot.send_message(message.chat.id, "Я и так уже был запущен")
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
 
-@bot.message_handler(commands=['stop'])
-def handle_text(message):
-    answer = "На что ты надеялся?"
-    log(message,answer)
-    bot.send_message(message.from_user.id, "На что ты надеялся? ")
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
 
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    answer = "Шо,каво"
-    if message.text == "а":
-        bot.send_message(message.from_user.id, "Зачем ты мне букву а отправил?")
-        answer = "Зачем ты мне букву а отправил?"
-        log(message,answer)
-    elif message.text == "0":
-        answer = "Это что,твой iq?"
-        log(message,answer)
-        bot.send_message(message.from_user.id, "Это что,твой iq?")
+bot = telebot.TeleBot(API_TOKEN)
+
+app = flask.Flask(__name__)
+
+
+# Empty webserver index, return nothing, just http 200
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return ''
+
+
+# Process webhook calls
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
     else:
-        bot.send_message(message.from_user.id, answer)
+        flask.abort(403)
 
 
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
 
 
+# Handle all other messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
 
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://salombot.herokuapp.com/") # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
-    bot.remove_webhook()
-    bot.polling(none_stop=True)
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+time.sleep(0.1)
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# Start flask server
+app.run(host=WEBHOOK_LISTEN,
+        port=WEBHOOK_PORT,
+        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+        debug=True)
