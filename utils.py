558c229ace680fc7019a1f63bc6adaecf84fed29
ibochchi2001 <46477510+ibochchi2001@users.noreply.github.com>
2019-01-19 13:17:52 +0500

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-from typing import Union
-
-
-def format_thousands(number: Union[int, float, str], sep: str = ',') -> str:
-    """Formats a number with a specified thousands separator"""
-    if isinstance(number, str):
-        number = float(number) if '.' in number else int(number)
-    return f'{number:_}'.replace('_', sep)
