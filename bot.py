39dfda86a28664c7f02ca4b4623514b990b045a1
ibochchi2001 <46477510+ibochchi2001@users.noreply.github.com>
2019-01-19 09:18:22 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,51 @@
-import os
-from flask import Flask, request
+import random
+import pickle
 
 import telebot
+from telebot import types
+from telebot.types import Message
 
 TOKEN = '665868259:AAEVV_-Zu4gsxyIUpcaj2Y5A9sXpViYSvy0'
+STICKER_ID = 'CAADAgADXwMAAgw7AAEKTh8jAAH9Q-gAAQI'
+
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+
+USERS = set()
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+@bot.message_handler(commands=['start', 'help'])
+def command_handler(message: Message):
+    bot.reply_to(message, 'There is no answer =(')
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
-    
-
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://salombot.herokuapp.com/' + TOKEN)
-    return "!", 200
+@bot.message_handler(content_types=['text'])
+@bot.edited_message_handler(content_types=['text'])
+def echo_digits(message: Message):
+    print(message.from_user.id)
+    if 'Alex Goodkid' in message.text:
+        bot.reply_to(message, 'Alex is good kid')
+        return
+    reply = str(random.random())
+    if message.from_user.id in USERS:
+        reply += f"  {message.from_user}, hello again"
+    bot.reply_to(message, reply)
+    USERS.add(message.from_user.id)
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+@bot.message_handler(content_types=['sticker'])
+def sticker_handler(message: Message):
+    bot.send_sticker(message.chat.id, STICKER_ID)
+
+
+@bot.inline_handler(lambda query: query.query)
+def query_text(inline_query):
+    print(inline_query)
+    try:
+        r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+        bot.answer_inline_query(inline_query.id, [r, r2])
+    except Exception as e:
+        print(e)
+
+
+bot.polling(timeout=60)
